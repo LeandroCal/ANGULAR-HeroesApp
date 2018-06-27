f8d36b86605c1c03304681f20c248f910ec28d28
LeandroCal <leancal13@hotmail.com>
2018-06-27 17:59:30 +0200

    App web de heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
+import { APP_ROUTING } from './app.routes';
+
+//servicios
+import { HeroesService } from './services/heroes.service';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
+import { FormsModule } from '@angular/forms';
+
+//pipes
+import { KeysPipe } from './pipes/keys.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent,
+    KeysPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    APP_ROUTING,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styles: []
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:any [] = [];
+  loading:boolean = true;
+
+  constructor( private _heroesService:HeroesService) {
+
+    this._heroesService.getHeroes()
+        .subscribe(data=>{
+
+          setTimeout( ()=>{
+            this.loading = false;
+            this.heroes = data;
+          }, 1000 );
+
+        });
+
+  }
+
+  ngOnInit() {
+  }
+
+  eliminaHeroe( key$:string ){
+    this._heroesService.eliminarHeroe(key$)
+        .subscribe(respuesta=>{
+          if(respuesta){
+            console.error(respuesta);
+          }else{
+            //todo bien
+            delete this.heroes[key$];
+          }
+          
+        })
+  }
+
+}
